629d973aecfed8a893b4a8c01c474a2a4eec29e4
Jesús David Mondragón Cirio <ao.ookami@gmail.com>
2019-02-23 23:23:38 -0600

    Se agregó el tope inferior y la siguiente figura con el color nuevo.

--- src/tetris.cpp
+++ src/tetris.cpp
@@ -13,6 +13,8 @@ using namespace sf;
 const int M = 20;
 const int N = 10;
 
+int field[M][N] = {0};
+
 struct Point { int x, y; } a[4], b[4];
 
 int figures[7][4] = {
@@ -24,6 +26,18 @@ int figures[7][4] = {
 	3,5,7,6, // J
 	2,3,4,5, // O
 };
+
+bool check() {
+	for(int i = 0; i < 4; i++) {
+		if(a[i].x < 0 || a[i].x > N || a[i].y >= M) {
+			return 0;
+		} else if(field[a[i].y][a[i].x]) {
+			return 0;
+		}
+	}
+
+	return 1;
+}
 
 int main() {
 	RenderWindow window(VideoMode(320, 480), "The game!");
@@ -38,6 +52,12 @@ int main() {
 	float timer = 0, delay = 0.3;
 
 	Clock clock;
+
+	int n = rand() % 7;
+	for(int i = 0; i < 4; i++) {
+		a[i].x = figures[n][i] % 2;
+		a[i].y = figures[n][i] / 2;
+	}
 
 	while(window.isOpen()) {
 		float time = clock.getElapsedTime().asSeconds();
@@ -55,7 +75,15 @@ int main() {
 			}
 		}
 
-		for(int i = 0; i < 4; i++) a[i].x += dx;
+		for(int i = 0; i < 4; i++) {
+			b[i] = a[i];
+			a[i].x += dx;
+		}
+		if(!check()) {
+			for(int i = 0; i < 4; i++) {
+				a[i] = b[i];
+			}
+		}
 
 		if(rotate) {
 			Point p = a[1];
@@ -65,30 +93,51 @@ int main() {
 				a[i].x = p.x - x;
 				a[i].y = p.y + y;
 			}
+
+			if(!check()) {
+				for(int i = 0; i < 4; i++) {
+					a[i] = b[i];
+				}
+			}
 		}
 
 		if(timer > delay) {
 			for(int i = 0; i < 4; i++) {
+				b[i] = a[i];
 				a[i].y += 1;
+			}
+			if(!check()) {
+				for(int i = 0; i < 4; i++) {
+					field[b[i].y][b[i].x] = colorNum;
+				}
+				colorNum = 1 + rand() % 7;
+				int n = rand() % 7;
+				for(int i = 0; i < 4; i++) {
+					a[i].x = figures[n][i] % 2;
+					a[i].y = figures[n][i] / 2;
+				}
 			}
 			timer = 0;
 		}
 
-		int n = 3;
-		if(a[0].x == 0) {
-			for(int i = 0; i < 4; i++) {
-				a[i].x = figures[n][i] % 2;
-				a[i].y = figures[n][i] / 2;
+		dx = 0; rotate = 0;  delay=0.3;
+		window.clear(Color::White);
+
+		for(int i = 0; i < M; i++) {
+			for(int j = 0; j < N; j++) {
+				if(field[i][j] == 0) {
+					continue;
+				}
+				s.setTextureRect(IntRect(field[i][j] * 18, 0, 18, 18));
+				s.setPosition(j * 18, i * 18);
+
+				window.draw(s);
 			}
 		}
 
-		dx = 0; rotate=0;
-
-		window.clear(Color::White);
-
 		for(int i = 0; i < 4; i++) {
+			s.setTextureRect(IntRect(colorNum * 18, 0, 18, 18));
 			s.setPosition(a[i].x * 18, a[i].y * 18);
-
 			window.draw(s);
 		}
 		window.display();
